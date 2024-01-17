9d4ad7997672df71fc0713cac6e24f7805764c15
James <james.kariuki2@student.moringaschool.com>
2024-01-17 10:27:35 +0300

    Draft

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('github-search-form');
+    const userInfoContainer = document.getElementById('user-list');
+    const userReposContainer = document.getElementById('user-repos');
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+    })
+}
+)
